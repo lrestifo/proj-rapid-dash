f31ff130ae60ee3b8902dcabe673d144fa1f4a39
Luciano Restifo <luciano.restifo@gmail.com>
2015-03-09 21:12:37 +0100

    Added spinner on data loading

--- js/proj_rapid_dash.js
+++ js/proj_rapid_dash.js
@@ -8,6 +8,33 @@
 // Create chart objects & link them to the page
 var dataTable = dc.dataTable("#bc-table");
 
+// Create the spinning wheel while waiting for data load
+// See http://fgnass.github.io/spin.js/
+var spinner = null;
+var spinner_div = 0;
+$(document).ready(function() {
+  var opts = {
+    lines: 13,        // The number of lines to draw
+    length: 20,       // The length of each line
+    width: 10,        // The line thickness
+    radius: 30,       // The radius of the inner circle
+    corners: 1,       // Corner roundness (0..1)
+    rotate: 0,        // The rotation offset
+    direction: 1,     // 1: clockwise, -1: counterclockwise
+    color: '#000',    // #rgb or #rrggbb or array of colors
+    speed: 1,         // Rounds per second
+    trail: 60,        // Afterglow percentage
+    shadow: false,    // Whether to render a shadow
+    hwaccel: true,    // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9,      // The z-index (defaults to 2000000000)
+    top: '50%',       // Top position relative to parent
+    left: '50%'       // Left position relative to parent
+  };
+  spinner_div = $('#spinner').get(0);
+  spinner = new Spinner(opts).spin(spinner_div);
+});
+
 // load data from the server
 d3.json("./data/tickets.json", function (data) {
 
@@ -45,6 +72,15 @@ d3.json("./data/tickets.json", function (data) {
 
   // Render the charts
   dc.renderAll();
+
+  for( var i = 0; i < 1000000; i++ ) {
+    for( var j = 0; j < 1000; j++ ) {}
+  }
+
+  // End the spinner
+  if( spinner != null ) {
+    spinner.stop(spinner_div);
+  }
 
 });
 
